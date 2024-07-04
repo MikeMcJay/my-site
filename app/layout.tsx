60909b970078d4cda7e020558cecb2123568a6dc
mikemcjay <jasonmcneillmail@gmail.com>
2024-07-04 17:52:00 +0100

    Added a custom favicon.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en">
+        <head>
+          <link rel="icon" href="/static/favicon.svg" sizes="any" />
+        </head>
         <body>{children}</body>
       </html>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import dynamic from 'next/dynamic'
 import React, { useCallback, useEffect, useState } from "react"
 import { SideNavBar, TopNavBar } from "../components/navbar"
 import AboutPanel from "../components/homepage/about"
@@ -9,14 +8,8 @@ import ContactPanel from "../components/homepage/contact"
 import { Footer } from "../components/footer"
 import { Diffusion } from "../components/homepage/diffusion"
 
-// const AboutPanel = dynamic(() => import("../components/homepage/about"));
-// const ProjectPanel = dynamic(() => import("../components/homepage/projects"));
-// const ContactPanel = dynamic(() => import("../components/homepage/contact"));
-
 export default function Page() {
     const [showSideBar, setShowSideBar] = useState(false);
-    const [showProjectPanel, setShowProjectPanel] = useState(false);
-    const [showContactPanel, setShowContactPanel] = useState(false);
 
     return (
         <div>

7297a3b000eb1d7de20a942b53f06729d2c4f16d
mikemcjay <jasonmcneillmail@gmail.com>
2024-07-13 18:21:09 +0100

    Added buttons to traverse through the list of project images.

--- components/page/imageCarousel.tsx
+++ components/page/imageCarousel.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ButtonCustom } from "../button";
 
 export default function ImageCarousel({
     projectImages
@@ -26,9 +27,42 @@ export default function ImageCarousel({
     return (
         <div className="image-carousel">
             <div
-                className="image-carousel-selected-image"
+                className="group image-carousel-selected-image"
                 style={{ backgroundImage: `url(${projectImages.get(selectedImage)})` }}
-            />
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="black" 
+                    className="image-carousel-next-image-left group-hover:visible"
+                    onClick={() => {
+                        Array.from(projectImages).map((image, index) => {
+                            if (image[0] === selectedImage) {
+                                var newIndex = (index - 1) % projectImages.size
+                                if (newIndex < 0) {
+                                    newIndex += projectImages.size
+                                }
+                                setSelectedImage(Array.from(projectImages)[newIndex][0]);
+                            }
+                        });
+                    }}
+                >
+                    <line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="black" 
+                    className="image-carousel-next-image-right group-hover:visible"
+                    onClick={() => {
+                        Array.from(projectImages).map((image, index) => {
+                            if (image[0] === selectedImage) {
+                                var newIndex = (index + 1) % projectImages.size
+                                if (newIndex < 0) {
+                                    newIndex += projectImages.size
+                                }
+                                setSelectedImage(Array.from(projectImages)[newIndex][0]);
+                            }
+                        });
+                    }}
+                >
+                    <line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline>
+                </svg>
+            </div>
             <div className="image-carousel-queued-scroll">
                 <div className="image-carousel-queued-container">
                     {Array.from(projectImages).map((image => {
